b9b6e1ddc39b4c02cb1adc160121fe11ae65e9d2
Falak Zahra <falak.zahra16@gmail.com>
2024-03-02 13:15:22 -0700

    changed background color

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const poems = [
     author: "Ernie",
     poem: "Roses are red \n Violets are blue \n Jerry Seinfeld is considering \n Making Bee Movie 2",
     image: "bee.jpeg",
-  },  
+  },
 ];
 
 let displayPoems = (array) => {
